608e36b354420b23c4458795f87648b6d7b555ad
Alpensin <Lotrus@yandex.ru>
2017-12-22 00:47:18 +0300

    Add files via upload
    Passwords generator

---
+++ generate_pass.py
@@ -0,0 +1,5 @@
+import string
+from random import choice
+
+def pass_generate(passqnty=1, passminlen=8):
+    return list("".join(choice(string.printable[:63]) for j in range(passminlen)) for i in range(passqnty))
