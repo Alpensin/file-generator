d4cb4cc33f164e11c18b5ad3f1376be703befe7a
Alpensin <Lotrus@yandex.ru>
2018-06-08 22:17:40 +0300

    uploaded script to make white background transparent

---
+++ img_background_to_transparent.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os, re
+
+def background_to_transparent(img_path, img_name, save_path):
+    img = Image.open(img_path)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+    newData = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+    img.putdata(newData)
+    img.save(save_path+img_name, "PNG")
+
+images_folder = "D:\\data\\temp\\coverage\\pci\\"
+new_folder = f"{images_folder}\\result"
+os.makedirs(new_folder)
+for img in os.listdir(images_folder):
+    if re.match(r".*(jpg|jpeg|gif|png)",img):
+        img_path = images_folder+img
+        background_to_transparent(img_path, img, images_folder)
